d2ecf0da6abd6e1f245403cb03132d78ce25b411
harfool gujjar <harfoolgujjar63@gmail.com>
2025-01-09 21:42:54 +0530

    font family

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+
+const Header = () => {
+  return (
+    <header>
+        <div className="top-strip py-2 ">
+        <div className="container">
+      <div className="flex items-center justify-between">
+        <div className="coll w-[50%] ">
+          <p className="text-[14px] text-blue-700" > get upto 50%</p>
+        </div>
+
+      </div>
+        </div>
+
+        </div>
+      </header>
+  )
+}
+
+export default Header
